082f4d02badffb79b398c9d03e21a9b7e31dfa64
artem_lazariev <lazarievartem25@gmail.com>
2026-07-15 20:00:53 +0200

    test

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Happy developing ✨')
+async function getLinks(link){
+    return await fetch(link).then(res => res.text()).catch(err => console.log(err));
+}
+for (let i of document.querySelectorAll("l")){
+    await getLinks(i.innerHTML).then(async res => i.outerHTML = res);
+}
